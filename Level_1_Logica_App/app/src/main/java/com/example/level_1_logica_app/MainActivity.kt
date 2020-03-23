cc8b721a9986049b895abbb072e944a04f173ebe
johnathan <xxsedohrxx@hotmail.com>
2020-03-23 09:53:15 +0100

    Readded task 1.2

--- Level_1_Logica_App/app/src/main/java/com/example/level_1_logica_app/MainActivity.kt
+++ Level_1_Logica_App/app/src/main/java/com/example/level_1_logica_app/MainActivity.kt
@@ -3,22 +3,39 @@ package com.example.level_1_logica_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
-    private var rightAnswers: Int = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_Enter.setOnClickListener {
+            onConfirmClick()
+        }
+    }
+
+    fun onConfirmClick(){
+
+        var answer1 = et_1.text.toString()
+        var answer2 = et_2.text.toString()
+        var answer3 = et_3.text.toString()
+        var answer4 = et_4.text.toString()
+
+        if (answer1 == "T" && answer2 == "F" && answer3 == "F" && answer4 == "F"){
+            onAnswerCorrect()
+        }else{
+            onAnswerincorrect()
+        }
 
     }
 
-    private fun checkAnswer(){
-
-
+    private fun onAnswerCorrect(){
+        Toast.makeText(this, getString(R.string.correct),Toast.LENGTH_LONG).show()
     }
 
+    private fun onAnswerincorrect(){
+        Toast.makeText(this, getString(R.string.incorrect),Toast.LENGTH_LONG).show()
+
+    }
 }
